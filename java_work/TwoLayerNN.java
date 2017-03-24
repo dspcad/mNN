8d627b8f5086e2201ecf9b8bf96ed1bdc56ebb38
DSPCAD <virtuoso.wu@gmail.com>
2017-03-24 13:29:31 -0500

    fix the input and dhidden bugs

--- java_work/TwoLayerNN.java
+++ java_work/TwoLayerNN.java
@@ -11,6 +11,7 @@ import java.util.Random;
 import Jama.Matrix;
 
 public class TwoLayerNN {
+    public static final int NUM_SAMPLE = 10000;
 
     public static void main(String[] args) throws IOException {
         int [] train_label_1 = new int[10000];
@@ -34,16 +35,17 @@ public class TwoLayerNN {
         //inputStream = new FileInputStream("../../cifar-10-batches-bin/data_batch_5.bin");
         //ArrayList<int[]> train_images_5 = batchRead(inputStream, train_label_5);
 
-        int num_sample = 10000;
-
         Matrix train_data_matrix = new Matrix(3072,10000);
-        Matrix weight_matrix_1   = new Matrix(100,3072);
-        Matrix weight_matrix_2   = new Matrix(10,100);
-        double [] b_1            = new double[100];
+        fillValuesToMatrix(train_data_matrix, train_images_1, 0);
+
+
+        Matrix weight_matrix_1   = new Matrix(50,3072);
+        Matrix weight_matrix_2   = new Matrix(10,50);
+        double [] b_1            = new double[50];
         double [] b_2            = new double[10];
 
-        initializeWeights(weight_matrix_1, 100, 3072, 10000);
-        initializeWeights(weight_matrix_2, 10,  100, 10000);
+        initializeWeights(weight_matrix_1, 50, 3072);
+        initializeWeights(weight_matrix_2, 10,  50);
         initializeBias(b_1);
         initializeBias(b_2);
         
@@ -65,27 +67,31 @@ public class TwoLayerNN {
         Matrix dhidden;
          
  
-        for(int i=0; i<10; i++){
-            hidden_layer = ReLU(weight_matrix_1, train_data_matrix, b_1);
+        for(int i=0; i<100; i++){
+            hidden_layer = ReLU(weight_matrix_1, train_data_matrix, b_1); //50 x 10000
             exp_scores = scoreFun(weight_matrix_2, hidden_layer, b_2);
-            interpretInProbs(exp_scores, num_sample);
-            data_loss = lossFun(exp_scores, train_label_1, num_sample);
+            interpretInProbs(exp_scores);
+            data_loss = lossFun(exp_scores, train_label_1);
             reg_loss = 0.5*reg_strength*regLoss(weight_matrix_2);
             loss = data_loss + reg_loss;
 
-            //if(i % 10 == 0)
+            if(i % 10 == 0)
                 System.out.println("iteration "+i+": loss: "+loss+" training accuracy: "+evalAccuracy(exp_scores, train_label_1));
             
+            
         
-            dscores = gradientComputation(exp_scores, train_label_1);
-            dW2 = dscores.times(hidden_layer.transpose());
-            dhidden = weight_matrix_2.transpose().times(dscores);
-            backPropgReLU(dhidden);
+            dscores = gradientComputation(exp_scores, train_label_1); //10 x 10000
+            dW2 = dscores.times(hidden_layer.transpose()); //10 x 50
+            db_2 = gradientBias(dscores, 10);
+
+            //if(i % 10 == 0)
+            //System.out.println("hidden layer "+calNonzero(hidden_layer));
+
+            dhidden = weight_matrix_2.transpose().times(dscores); //50 x 10000
+            backPropgReLU(dhidden, hidden_layer);
 
             dW1 = dhidden.times(train_data_matrix.transpose());
-
-            db_2 = gradientBias(dscores, 10);
-            db_1 = gradientBias(dhidden, 100);
+            db_1 = gradientBias(dhidden, 50);
             
             addRegGradientContrbution(dW2, weight_matrix_2, reg_strength);
             addRegGradientContrbution(dW1, weight_matrix_1, reg_strength);
@@ -100,14 +106,28 @@ public class TwoLayerNN {
         
     }
 
-    public static void backPropgReLU(Matrix M){
+    public static int calNonzero(Matrix M){
         int row = M.getRowDimension();
         int col = M.getColumnDimension();
+        int sum = 0;
 
         for(int i=0; i<row; i++)
             for(int j=0; j<col; j++)
-                if(M.get(i,j) <= 0){
-                    M.set(i,j, 0);
+                if(M.get(i,j)>0)
+                    sum++;
+
+        return sum;
+          
+    }
+
+    public static void backPropgReLU(Matrix dH, Matrix H){
+        int row = dH.getRowDimension();
+        int col = dH.getColumnDimension();
+
+        for(int i=0; i<row; i++)
+            for(int j=0; j<col; j++)
+                if(H.get(i,j) <= 0){
+                    dH.set(i,j, 0.0);
                     //System.out.println("dW: ");
                 }
     }
@@ -149,10 +169,11 @@ public class TwoLayerNN {
 
     public static double [] gradientBias(Matrix M, int length){
         double [] db = new double[length];
-        double sum = 0;
+        double sum;
 
         for(int i=0;i<length;i++){
-            for(int j=0;j<10000;j++)
+            sum = 0;
+            for(int j=0;j<NUM_SAMPLE;j++)
                 sum += M.get(i,j);
 
             db[i] = sum;
@@ -162,14 +183,16 @@ public class TwoLayerNN {
     }
 
     public static Matrix gradientComputation(double [][] exp_scores, int [] train_label){
-        Matrix dscores = new Matrix(10, 10000);
+        Matrix dscores = new Matrix(10, NUM_SAMPLE);
 
-        for(int j=0; j<train_label.length;j++){
+        for(int j=0; j<NUM_SAMPLE;j++){
             for(int i=0;i<10;i++){
-                if(i == train_label[j])
-                    dscores.set(i, j, (exp_scores[i][j] - 1)/10000);
+                if(i == train_label[j]){
+                    dscores.set(i, j, (exp_scores[i][j] - 1)/NUM_SAMPLE);
+                    //System.out.println("prob: "+dscores.get(i, j));
+                }
                 else 
-                    dscores.set(i, j, exp_scores[i][j]/10000);
+                    dscores.set(i, j, exp_scores[i][j]/NUM_SAMPLE);
             }
         }
 
@@ -217,60 +240,72 @@ public class TwoLayerNN {
     }
 
 
-    public static double lossFun(double [][] exp_scores, int [] train_label, int num_sample){
+    public static double lossFun(double [][] exp_scores, int [] train_label){
         double sum = 0;
 
-        for(int i=0;i<num_sample;i++)
+        for(int i=0;i<NUM_SAMPLE;i++)
             sum -= Math.log(exp_scores[ train_label[i] ][i]);
         
-        return sum/num_sample;
+        return sum/NUM_SAMPLE;
     }
 
 
-    public static void interpretInProbs(double [][] exp_scores, int num_sample){
-        double sum = 0;
+    public static void interpretInProbs(double [][] exp_scores){
+        double sum;
 
-        for(int j=0;j<num_sample;j++){
+        for(int j=0;j<NUM_SAMPLE;j++){
+            sum = 0;
             for(int i=0;i<10;i++)
                 sum += exp_scores[i][j];
 
            
-            for(int i=0;i<10;i++)
+            for(int i=0;i<10;i++){
                 exp_scores[i][j] = exp_scores[i][j]/sum;
-
-            sum = 0;
+                //System.out.println("prob: "+exp_scores[i][j]);
+            }
         }
 
     }
 
     public static double [][] scoreFun(Matrix W, Matrix H, double [] b){
-        Matrix dot_product = W.times(H);
-        double [][] scores = new double[b.length][dot_product.getColumnDimension()];
+        Matrix result = W.times(H);
+        double [][] scores = new double[b.length][result.getColumnDimension()];
 
         for(int i=0;i<b.length;i++)
-            for(int j=0;j<dot_product.getColumnDimension();j++)
-                scores[i][j] =Math.exp(dot_product.get(i,j) + b[i]);
+            for(int j=0;j<result.getColumnDimension();j++){
+                scores[i][j] =Math.exp(result.get(i,j) + b[i]);
+                //scores[i][j] = result.get(i,j) + b[i];
+                //System.out.println("score: "+scores[i][j]);
+
+            }
 
         return scores;            
     }
 
     public static Matrix ReLU(Matrix W, Matrix X, double [] b){
-        Matrix dot_product = W.times(X);
-        int row = dot_product.getRowDimension();
-        int col = dot_product.getColumnDimension();
+        Matrix result = W.times(X);
+        int row = result.getRowDimension();
+        int col = result.getColumnDimension();
+        //System.out.println("row: "+row);
+        //System.out.println("col: "+col);
         double max_val = 0;
 
         for(int i=0; i<row; i++)
             for(int j=0; j<col; j++){
-                max_val = dot_product.get(i,j)+b[i];
-                dot_product.set(i,j, max_val > 0 ? max_val : 0.0);
+                max_val = result.get(i,j)+b[i];
+                if(max_val > 0){
+                    result.set(i,j, max_val);
+                    //System.out.println("max: "+max_val);
+                }
+                else
+                    result.set(i,j, 0.0);
 
             }
         
 
         //System.out.println("row dim: "+dot_product.getRowDimension());
         //System.out.println("col dim: "+dot_product.getColumnDimension());
-        return dot_product;
+        return result;
     }
 
 
@@ -280,12 +315,12 @@ public class TwoLayerNN {
     }
 
 
-    public static void initializeWeights(Matrix W, int row, int col, int num_samples){
+    public static void initializeWeights(Matrix W, int row, int col){
         Random normal_var = new Random();
 
         for(int i=0; i<row; i++)
             for(int j=0; j<col; j++)
-                W.set(i, j, 0.01*normal_var.nextGaussian()/num_samples); 
+                W.set(i, j, 0.01*normal_var.nextGaussian()/Math.sqrt(NUM_SAMPLE)); 
 
         //System.out.println("W[0][0]: "+W.get(0,0));
     }
@@ -293,12 +328,14 @@ public class TwoLayerNN {
 
     public static void fillValuesToMatrix(Matrix train_data_matrix, ArrayList<int[]> train_images, int ref_pt){
         int [] image_pixel;
-        double val = 0;
-        for(int j=0;j<10000;j++)
-            for(int i=0;i<3072;i++){
-                image_pixel = train_images.get(j);
-                train_data_matrix.set(i, ref_pt*10000+j, image_pixel[i]);
-            }
+
+        for(int j=0;j<NUM_SAMPLE;j++){
+            image_pixel = train_images.get(j);
+            for(int i=0;i<3072;i++)
+                train_data_matrix.set(i, ref_pt*NUM_SAMPLE+j, image_pixel[i]);
+        }
+
+        //System.out.println("pixel(3071): "+train_data_matrix.get(3071,9999));
     }
 
     public static ArrayList<int[]> batchRead(FileInputStream inputStream, int [] train_labels) throws IOException{
@@ -319,16 +356,23 @@ public class TwoLayerNN {
                             b[1 + 1024 * 1 + row * 32 + col] & 0xFF,
                             b[1 + 1024 * 2 + row * 32 + col] & 0xFF);
                   
-                    pixels[row*32+col*3+0] = color.getRed();  
-                    pixels[row*32+col*3+1] = color.getGreen();  
-                    pixels[row*32+col*3+2] = color.getBlue();  
+                    //pixels[row*32+col*3+0] = color.getRed();  
+                    //pixels[row*32+col*3+1] = color.getGreen();  
+                    //pixels[row*32+col*3+2] = color.getBlue();  
+
+                    pixels[row*32+col+0*32*32] = color.getRed();  
+                    pixels[row*32+col+1*32*32] = color.getGreen();  
+                    pixels[row*32+col+2*32*32] = color.getBlue();  
+
+                   
                 }
             }
+            //System.out.println("pixel(3071): "+pixels[3071]);
             train_images.add(pixels);
             i++;
         } while(inputStream.read(b) != -1);
             
-        System.out.println("size is "+train_labels.length);
+        System.out.println("size is "+train_images.size());
 
         return train_images;
     }
